84306a0352a3971845982c66650fddc6977ab69e
PeterBlacken <ppgaldames@gmail.com>
2025-04-16 08:49:41 -0400

    Created time reset. next task, implment with BLE

---
+++ main/src/slave/main/BLE_config.h
@@ -0,0 +1,56 @@
+//#include <esp32-hal-bt.c>
+#include <NimBLEDevice.h>
+#define	size_of_struct 12000
+#define  chunk_size 128
+
+
+const char* matrixUUID= "180C";
+//BLEService sensorService(matrixUUID);
+//BLEStringCharacteristic sensorCharacteristic(matrixUUID,BLERead | BLENotify,128);
+
+// Creation of the server, service and characteristic for sending data
+static NimBLEServer* pServer = nullptr;
+static NimBLEService* pSensorService = nullptr;
+static NimBLECharacteristic* pSensorCharacteristic = nullptr;
+
+void Init_BLE()
+{
+
+	
+
+  NimBLEDevice::init("Timer-Test");
+
+  NimBLEDevice::setMTU(256); //setMTU after init the BLEDEVICE XDD else crash 
+	if (!NimBLEDevice::setDeviceName("Timer-Test")) 
+	{Serial.print("Name not changed");}
+  pServer = NimBLEDevice::createServer();
+
+  // Crear servicio
+  pSensorService = pServer->createService(matrixUUID);
+
+  // Crear característica con notificación
+  pSensorCharacteristic = pSensorService->createCharacteristic(
+    matrixUUID,
+    NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY
+  );
+
+  // Valor inicial
+  pSensorCharacteristic->setValue("0");
+
+  // Iniciar el servicio
+  pSensorService->start();
+
+  // Configurar publicidad
+  NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(pSensorService->getUUID());
+  ////pAdvertising->setScanResponse(true);
+  pAdvertising->enableScanResponse(true);
+  pAdvertising->start();
+
+  Serial.println("IMU Peripheral (Sending Data) - NimBLE iniciado");
+  Serial.printf("MTU = %i",NimBLEDevice::getMTU() );
+
+}
+
+
+
--- main/src/slave/main/main.ino
+++ main/src/slave/main/main.ino
@@ -1,2 +1,56 @@
+// includes 
+#include "utils.h"
+//#include "BLE_config.h"
 
-#include <arduino>
+//#include <arduino>
+//#include 
+//#include <esp_sntp.h>
+#include <esp_timer.h>
+//#include <rtc.h>
+// firts try. the idea is to let the clock works but after 10 secs. restart it. so we know we could reset it properly
+// esta funcion parece prometedora esp_time_impl_set_boot_time(now - since_boot)
+// la funcion anterior no fue la mejor opcion debido a que es una funcion privada de la api
+// por lo que mejor usaré un tiempo que se definirá como "0" cuando se reciva una señal. Esto signifiva que el sistema tendrá su punto de boot inicial intacto y otro tiempo tomado como cero parcial.
+// la implementación de el tiempo parcial funcionó!
+//
+// Ahora la idea es implmentarla en el envio de datos BLUEthoot y que este se active cuando se envie una señal. 
+//
+//struct timeval tv_now;
+
+static uint64_t time_cero= 0;
+static uint64_t time_actual=0;
+/*
+int64_t get_time_us()
+{
+    gettimeofday(&tv_now, NULL);
+    int64_t time_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+    return time_us;
+}
+*/
+void setup()
+{
+  time_cero= esp_timer_get_time();
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.print("Restart timer test");
+
+
+}
+
+void loop()
+{
+  uint16_t counter= 0;
+  time_actual=esp_timer_get_time();
+  Serial.printf("Time uS= %llu \n ", time_actual-time_cero);
+  //delay(100);
+
+  if (time_actual - time_cero >= 10000000)
+  {
+    time_cero=esp_timer_get_time(); 
+    Serial.println("Clock reset");
+  }
+  //else
+  //{
+  //  counter=counter+1;
+  //}
+}
